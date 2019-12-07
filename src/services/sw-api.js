6b3f997d5ba6a9d13f973412cc587a3df7c78f07
Carol Sanders <carol@carolsanders.org>
2019-12-07 14:00:12 -0800

    Update sw-api.js

--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -5,3 +5,9 @@ export function getAllStarships() {
     fetch(url, { mode: 'cors'})
     .then(res => res.json())
   )};
+
+export async function getPilots(urls) {
+  const promises = urls.map(url => fetch(url).then(res => res.json()));
+  const pilotObjects = await Promise.all(promises);
+  return pilotObjects;
+}
